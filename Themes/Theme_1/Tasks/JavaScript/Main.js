f8f698dc9db08c7f8a6f5e7fdc908f1d8ed7ed5a
TMOLIIL <themeaningofliveisinlove@gmail.com>
2026-01-21 23:13:02 +0300

    Update Main.js

--- Themes/Theme_1/Tasks/JavaScript/Main.js
+++ Themes/Theme_1/Tasks/JavaScript/Main.js
@@ -11,3 +11,16 @@ function Answer_1(id)
         alert("Неправильно.");
     }
 }
+function Answer_2(id)
+{
+    event.preventDefault();
+    Task_Answer_2 = document.querySelector(`[name="${id}"]:checked`).value;
+    if (Task_Answer_2 == 2)
+    {
+        alert("Правильно!");
+    }
+    else
+    {
+        alert("Неправильно.");
+    }
+}
