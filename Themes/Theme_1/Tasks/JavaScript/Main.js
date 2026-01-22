8795daeadaa20a4dd366e73d426c0f34d13b3fc9
TMOLIIL <themeaningofliveisinlove@gmail.com>
2026-01-22 13:50:00 +0300

    Update Main.js

--- Themes/Theme_1/Tasks/JavaScript/Main.js
+++ Themes/Theme_1/Tasks/JavaScript/Main.js
@@ -37,3 +37,89 @@ function Answer_3(id)
         alert("Неправильно.");
     }
 }
+function Answer_4(id)
+{
+    event.preventDefault();
+    Task_Answer_4 = document.querySelector(`[name="${id}"]:checked`).value;
+    if (Task_Answer_4 == 1)
+    {
+        alert("Правильно!");
+    }
+    else
+    {
+        alert("Неправильно.");
+    }
+}
+function Answer_5(id)
+{
+    event.preventDefault();
+    Task_Answer_5 = document.querySelector(`[name="${id}"]:checked`).value;
+    if (Task_Answer_5 == 2)
+    {
+        alert("Правильно!");
+    }
+    else
+    {
+        alert("Неправильно.");
+    }
+} function Answer_6(id)
+{
+    event.preventDefault();
+    Task_Answer_6 = document.querySelector(`[name="${id}"]:checked`).value;
+    if (Task_Answer_6 == 3)
+    {
+        alert("Правильно!");
+    }
+    else
+    {
+        alert("Неправильно.");
+    }
+} function Answer_7(id)
+{
+    event.preventDefault();
+    Task_Answer_7 = document.querySelector(`[name="${id}"]:checked`).value;
+    if (Task_Answer_7 == 1)
+    {
+        alert("Правильно!");
+    }
+    else
+    {
+        alert("Неправильно.");
+    }
+} function Answer_8(id)
+{
+    event.preventDefault();
+    Task_Answer_8 = document.querySelector(`[name="${id}"]:checked`).value;
+    if (Task_Answer_8 == 2)
+    {
+        alert("Правильно!");
+    }
+    else
+    {
+        alert("Неправильно.");
+    }
+} function Answer_9(id)
+{
+    event.preventDefault();
+    Task_Answer_9 = document.querySelector(`[name="${id}"]:checked`).value;
+    if (Task_Answer_9 == 3)
+    {
+        alert("Правильно!");
+    }
+    else
+    {
+        alert("Неправильно.");
+    }
+} function Answer_10(id)
+{
+    event.preventDefault();
+    Task_Answer_10 = document.querySelector(`[name="${id}"]:checked`).value;
+    if (Task_Answer_10 == 1)
+    {
+        alert("Правильно!");
+    }
+    else
+    {
+        alert("Неправильно.");
+    }
+}
