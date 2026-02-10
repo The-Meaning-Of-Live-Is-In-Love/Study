c27b944a2347a15f216dad58512d77eea44091e2
TMOLIIL <themeaningofliveisinlove@gmail.com>
2026-02-10 10:53:05 +0300

    Update Main.js

--- JavaScript/Main.js
+++ JavaScript/Main.js
@@ -123,3 +123,128 @@ function Answer_1_5(id)
         alert("Неправильно.");
     }
 }
+function Answer_1_1(id)
+{
+    event.preventDefault();
+    Task_Answer_1_1 = document.querySelector(`[name="${id}"]:checked`).value;
+    if (Task_Answer_1_1 == 1)
+    {
+        alert("Правильно!");
+    }
+    else
+    {
+        alert("Неправильно.");
+    }
+}
+function Answer_1_2(id)
+{
+    event.preventDefault();
+    Task_Answer_1_2 = document.querySelector(`[name="${id}"]:checked`).value;
+    if (Task_Answer_1_2 == 2)
+    {
+        alert("Правильно!");
+    }
+    else
+    {
+        alert("Неправильно.");
+    }
+}
+function Answer_1_3(id)
+{
+    event.preventDefault();
+    Task_Answer_1_3 = document.querySelector(`[name="${id}"]:checked`).value;
+    if (Task_Answer_1_3 == 3)
+    {
+        alert("Правильно!");
+    }
+    else
+    {
+        alert("Неправильно.");
+    }
+}
+function Answer_1_4(id)
+{
+    event.preventDefault();
+    Task_Answer_1_4 = document.querySelector(`[name="${id}"]:checked`).value;
+    if (Task_Answer_1_4 == 1)
+    {
+        alert("Правильно!");
+    }
+    else
+    {
+        alert("Неправильно.");
+    }
+}
+function Answer_1_5(id)
+{
+    event.preventDefault();
+    Task_Answer_1_5 = document.querySelector(`[name="${id}"]:checked`).value;
+    if (Task_Answer_1_5 == 2)
+    {
+        alert("Правильно!");
+    }
+    else
+    {
+        alert("Неправильно.");
+    }
+} function Answer_1_6(id)
+{
+    event.preventDefault();
+    Task_Answer_1_6 = document.querySelector(`[name="${id}"]:checked`).value;
+    if (Task_Answer_1_6 == 3)
+    {
+        alert("Правильно!");
+    }
+    else
+    {
+        alert("Неправильно.");
+    }
+} function Answer_1_7(id)
+{
+    event.preventDefault();
+    Task_Answer_1_7 = document.querySelector(`[name="${id}"]:checked`).value;
+    if (Task_Answer_1_7 == 1)
+    {
+        alert("Правильно!");
+    }
+    else
+    {
+        alert("Неправильно.");
+    }
+} function Answer_1_8(id)
+{
+    event.preventDefault();
+    Task_Answer_1_8 = document.querySelector(`[name="${id}"]:checked`).value;
+    if (Task_Answer_1_8 == 2)
+    {
+        alert("Правильно!");
+    }
+    else
+    {
+        alert("Неправильно.");
+    }
+} function Answer_9(id)
+{
+    event.preventDefault();
+    Task_Answer_9 = document.querySelector(`[name="${id}"]:checked`).value;
+    if (Task_Answer_9 == 3)
+    {
+        alert("Правильно!");
+    }
+    else
+    {
+        alert("Неправильно.");
+    }
+} function Answer_1_10(id)
+{
+    event.preventDefault();
+    Task_Answer_1_10 = document.querySelector(`[name="${id}"]:checked`).value;
+    if (Task_Answer_1_10 == 1)
+    {
+        alert("Правильно!");
+    }
+    else
+    {
+        alert("Неправильно.");
+    }
+}
