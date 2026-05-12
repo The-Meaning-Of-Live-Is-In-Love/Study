77e414f049776a59cc07e3a0966aa0a77a73bf55
TMOLIIL <themeaningofliveisinlove@gmail.com>
2026-05-12 20:53:26 +0300

    Add files via upload

--- JavaScript/Colors.js
+++ JavaScript/Colors.js
@@ -6,13 +6,13 @@ if(localStorage.getItem('--Color_Background') == 'black')
 {
     document.documentElement.style.setProperty('--Color_Background', 'black');
 }
-if(localStorage.getItem('--Color_Text') == 'white')
-{
-    document.documentElement.style.setProperty('--Color_Text', 'white');
-}
 if(localStorage.getItem('--Color_Text') == 'black')
 {
     document.documentElement.style.setProperty('--Color_Text', 'black');
+}
+if(localStorage.getItem('--Color_Text') == 'white')
+{
+    document.documentElement.style.setProperty('--Color_Text', 'white');
 }
 if(localStorage.getItem('--Color_Complementary') == 'cyan')
 {
@@ -21,4 +21,20 @@ if(localStorage.getItem('--Color_Complementary') == 'cyan')
 if(localStorage.getItem('--Color_Complementary') == 'pink')
 {
     document.documentElement.style.setProperty('--Color_Complementary', 'pink');
+}
+if(localStorage.getItem('--Color_Complementary_1') == '255, 203, 219')
+{
+    document.documentElement.style.setProperty('--Color_Complementary', '255, 203, 219');
+}
+if(localStorage.getItem('--Color_Complementary_2') == '250, 218, 221')
+{
+    document.documentElement.style.setProperty('--Color_Complementary', '250, 218, 221');
+}
+if(localStorage.getItem('--Color_Complementary_1') == '127, 215, 245')
+{
+    document.documentElement.style.setProperty('--Color_Complementary', '127, 215, 245');
+}
+if(localStorage.getItem('--Color_Complementary_2') == '192, 235, 250')
+{
+    document.documentElement.style.setProperty('--Color_Complementary', '192, 235, 250');
 }
--- JavaScript/Main.js
+++ JavaScript/Main.js
@@ -1,13 +1,9 @@
-function Switching_Theme(id)
+﻿function Switching_Theme(id)
 {
     event.preventDefault();
     Theme_Type = document.querySelector(`[name = "${id}"]:checked`).value;
     if (Theme_Type == 1)
     {
-        document.documentElement.style.setProperty('--Color_Background', 'white');
-        localStorage.setItem('--Color_Background', 'white');
-        document.documentElement.style.setProperty('--Color_Text', 'black');
-        localStorage.setItem('--Color_Text', 'black');
         document.documentElement.style.setProperty('--Color_Background', 'white');
         localStorage.setItem('--Color_Background', 'white');
         document.documentElement.style.setProperty('--Color_Text', 'black');
@@ -29,11 +25,19 @@ function Switching_Color_Complementary(id)
     {
         document.documentElement.style.setProperty('--Color_Complementary', 'cyan');
         localStorage.setItem('--Color_Complementary', 'cyan');
+        document.documentElement.style.setProperty('--Color_Complementary_1', '127, 215, 245');
+        localStorage.setItem('--Color_Complementary_1', '127, 215, 245');
+        document.documentElement.style.setProperty('--Color_Complementary_2', '192, 235, 250');
+        localStorage.setItem('--Color_Complementary_2', '192, 235, 250');
     }
     if (Color_Complementary_Type == 2)
     {
         document.documentElement.style.setProperty('--Color_Complementary', 'pink');
         localStorage.setItem('--Color_Complementary', 'pink');
+        document.documentElement.style.setProperty('--Color_Complementary_1', '255, 203, 219');
+        localStorage.setItem('--Color_Complementary_1', '255, 203, 219');
+        document.documentElement.style.setProperty('--Color_Complementary_2', '250, 218, 221');
+        localStorage.setItem('--Color_Complementary_2', '250, 218, 221');
     }
 }
 function Answer_1_1(id)
